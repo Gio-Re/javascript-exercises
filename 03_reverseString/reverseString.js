cf9599229f5ba6043fce9bb54597a11029a98f3a
Gio-Re <181334102+Gio-Re@users.noreply.github.com>
2025-03-28 19:52:43 +0100

    Create reverse string function
    split the assigned strig with the space in an array, reverse the array and iterate a for loop throght it to concacatenerte in the reversed string
    the single char

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const textArray = text.split('');
+    const reversedArray = textArray.reverse();
+    let reversed = '';
+    for  (const char of reversedArray) {
+        reversed += char;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
