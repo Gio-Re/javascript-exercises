2eacc79b93140dfc11bef0beb36b51b90aaf90bb
Gio-Re <181334102+Gio-Re@users.noreply.github.com>
2025-03-29 11:40:32 +0100

    Create a Function to sum the num between 2 integer
    insert condition to verify if are int and num, and condition to determinate che greater number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    let sum = 0;
+    if (start < 0 || stop <0 || !Number.isInteger(start) || !Number.isInteger(stop)) {
+        return 'ERROR';
+    }
+    else if ( start > stop ) {
+        for (let num = stop; num <= start; num++) {
+            sum += num;
+        }
+        return sum;
+    
+    }
+    for (let num = start; num <= stop; num++) {
+        sum += num;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
